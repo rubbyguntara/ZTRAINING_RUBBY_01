53575eadab357ffbaa7c81395d6a1e4da0ca1144
rubbyguntara <distortion4by@gmail.com>
2021-12-15 15:06:16 +0700

    Display invoice list using json

--- webapp/controller/View1.controller.js
+++ webapp/controller/View1.controller.js
@@ -1,9 +1,16 @@
 sap.ui.define([
-	"sap/ui/core/mvc/Controller"
-], function(Controller) {
+	"sap/ui/core/mvc/Controller",
+	"sap/m/MessageToast"
+	
+], function(Controller, MessageToast) {
 	"use strict";
 
 	return Controller.extend("xxxnamespaceZTRAINING_RUBBY_01.controller.View1", {
-
+		onShowHello: function() {
+			var a = "Hello World action from controller";
+			// alert(a);
+			// console.log(a);
+			MessageToast.show(a);
+		}
 	});
 });
